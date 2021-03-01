920552c0c4e37e3749b0b63508e4eeae60c5c5f4
Ryan Lee <rjlguitar@gmail.com>
2021-03-01 10:28:48 -0800

    added api fetch call

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ currency_fromEl.addEventListener('change', convert);
 amount_fromEl.addEventListener('input', convert);
 currency_toEl.addEventListener('change', convert);
 amount_toEl.addEventListener('input', convert);
+
+ex.addEventListener('click', () => {
+  const val = currency_fromEl.value;
+  currency_fromEl.value = currency_toEl.value;
+  currency_toEl.value = val;
+  convert();
+});
+
+function convert() {
+  const from_cr = currency_fromEl.value;
+  const to_cr = currency_toEl.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${from_cr}`)
+    .then((res) => res.json())
+    .then((res) => {
+      const rate = res.rates[to_currency];
+      rateEl.innerText = `1 ${from_cr} = ${rate} ${to_cr}`;
+      amount_toEl.value = (amount_fromEl.value * rate).toFixed(2);
+    });
+}
+
+convert();
